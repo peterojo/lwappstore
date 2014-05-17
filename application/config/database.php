80b99e0ea543dea8a5a762508639f24d10e50242
petrelli22 <petrelli22@users.noreply.github.com>
2014-05-17 07:25:47 -0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'pastorchrisonline.org';
-$db['default']['username'] = 'usr_vidshare';
-$db['default']['password'] = 'usr_vidshare900';
-$db['default']['database'] = 'db_vidshare';
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'xxxxxxxxx';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
